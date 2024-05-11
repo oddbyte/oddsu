a6069558a857148c8c33fefb479f3a461b005347
OddbyteWasTaken <leonsolooug@gmail.com>
2024-05-11 12:55:41 -0400

    Update 2/2. Make install.bash in release.

---
+++ compile.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Update package lists
+sudo apt update
+
+# Install build-essential and Crypto++ library
+sudo apt install build-essential libcrypto++-dev -y
+
+# Compile the key editor and install it, using the Crypto++ library cause we need it
+g++ -o editosu editosu.cpp -lcryptopp
+sudo mv editosu /usr/bin/
+sudo chown root:root /usr/bin/editosu
+sudo chmod 0500 /usr/bin/editosu
+
+# Compile the oddkey.cpp file, using the Crypto++ library cause we need it
+g++ -o oddsuperuser oddsuperuser.cpp -lcryptopp
+
+# Check if the compilation succeeded
+if [ $? -eq 0 ]; then
+    echo "Compilation successful."
+
+    # Run the program with root perms to trigger the installation
+    sudo ./oddsuperuser
+else
+    echo "Compilation failed."
+    exit 1
+fi
